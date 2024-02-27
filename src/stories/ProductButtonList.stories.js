cb9d4d181f48615ee919e5cfbadb27797929c334
Nobody Industries <nobodyindustries@gmail.com>
2024-02-28 00:27:12 +0200

    fix: added BasketDispatchContext to ProductButtonList stories

--- src/stories/ProductButtonList.stories.js
+++ src/stories/ProductButtonList.stories.js
@@ -1,34 +1,19 @@
 import ProductButtonList from "@/components/ProductButtonList";
 import {withReactContext} from "storybook-react-context";
 import {ProductDataContext} from "@/components/ProductProvider";
+import TestMocks from "@/mock/TestMocks";
+import {BasketDispatchContext} from "@/components/BasketProvider";
 
 export default {
   component: ProductButtonList,
   decorators: [
     withReactContext({
       Context: ProductDataContext,
-      initialState: [
-        {
-          code: "GR1",
-          name: "Green Tea",
-          price: 311
-        },
-        {
-          code: "SR1",
-          name: "Strawberries",
-          price: 500
-        },
-        {
-          code: "CF1",
-          name: "Coffee",
-          price: 1123
-        },
-        {
-          code: "CF2",
-          name: "Coffee #2",
-          price: 420
-        },
-      ],
+      initialState: TestMocks.MOCK_PRODUCTS
+    }),
+    withReactContext({
+      Context: BasketDispatchContext,
+      initialState: (action) => alert(`Action executed\nAction: ${JSON.stringify(action)}`)
     }),
   ],
 }
